4939e11bfd8f907f9228184373b258d1739eedff
koa2019 <no-reply>
2023-09-21 18:59:00 -0700

    finished assignment 3 problem 2 from codeforces.com
    Signed-off-by: koa2019 <no-reply>

--- assignment3/a3-1/main.cpp
+++ assignment3/a3-1/main.cpp
@@ -1,19 +1,56 @@
 #include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int main(int argc, char** argv) {
 
-    int t=1, n=2;
-    //vector<int> a = {24, 43, 6, 991};
+    int t=6, n=2;
+    vector<string> v = {"james123","sebastian609","james123","garlic3","james123","sebastian609"};
+    vector<string> list;
+    vector<string>::iterator itr;
     
-    cin>>t;  //[1,10] Enter number of test cases
+    //cout<<"Enter number of names\n";
+    cin>>t;  //[1,10] Enter number of test cases    
+    string a[t]={};
+    string str="", msg=""; 
     
-    for(int i=1; i<=t; i++) {
-       
-        cin>>n; //Array size or cost [2, 10^]   
-        //n=a[i-1]; 
+    for(int i=0; i<t; i++) {
+        //cout<<"Enter a name\n";
+        cin>>str;
+        //str=v[i];
         
-             
-    }   
+        for(int j=0;j<=i;j++){
+            
+            //cout<<"i="<<i<<" j="<<j<<"  "<<str<<"=="<<a[j]<<"  ";
+            
+            if(str.compare(a[j])==0){ // strings are equal
+                msg="YES\n";
+                break;
+            } else { 
+                if(j==i){a[i]=str;}  
+                msg="NO\n";
+            }                 
+        }
+        cout<<msg;        
+    }
     return 0;
 }
+
+//    for(int i=0; i<t; i++) {
+//        
+//        string str="";
+//        //cout<<"Enter a name\n";
+//        //cin>>str;
+//        
+//        list.push_back(str);
+//        for(int i=0; i<list.size();i++){
+//            
+//            cout<<i<<" "<<list[i]<<" ";
+//            
+//            itr = find(list.begin(),list.end(),list[i]);
+//            
+//            if(itr != list.end()){ // if name is found
+//                cout<<"YES\n";
+//            } else { cout<<"NO\n"; }     
+//        }             
+//    }   
--- assignment3/a3-3/main.cpp
+++ assignment3/a3-3/main.cpp
@@ -1,3 +1,15 @@
+/* C. Highest Frequency Element
+ * 
+ * Given an array of size n, find which element occurs the most in the array. 
+ * If two elements are tied for the highest frequency, the answer is the one with the higher value.
+   Input:
+        * The first line of input contains 𝑡 (1 ≤ 𝑡 ≤ 20), the number of queries to answer.
+        * The first line of each query contains 𝑛 (1 ≤ 𝑛 ≤ 100), the size of the array.
+        * On the next line there will be 𝑛 numbers.
+        * The 𝑖-th number contains 𝑎𝑖 (1 ≤ 𝑎𝑖 ≤ 100) , the value of the 𝑖-th element.
+Output:
+ * For each query, first output the value of the element that occurs the most times, then print its frequency on the same line.
+*/
 #include <bits/stdc++.h>
 using namespace std;
 
